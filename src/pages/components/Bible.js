ef7c9722761c4d622474e3072d61f435a2964a6b
jazteng2 <jaredteng1602@gmail.com>
2023-12-12 20:29:25 +1100

    update

--- src/pages/components/Bible.js
+++ src/pages/components/Bible.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
+
 export default function Bible() {
     const [currentVerse, setCurrentVerse] = useState(1);
+
     function handleSubmit(e) {
         e.preventDefault();
-
+        
     }
 
     return (
         <section className="bibleDisplay">
-            <form action={handleSubmit}></form>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="verseSearch" placeholder="Search: ctrl + /"/>
+                <input type="submit" />
+            </form>
             <div className="verses"></div>
         </section>
     );
