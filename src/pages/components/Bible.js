318479da1de01fd881bc92d303f61f09d5239043
jazteng2 <jaredteng1602@gmail.com>
2023-12-14 15:03:29 +1100

    center selected verse incomplete

--- src/pages/components/Bible.js
+++ src/pages/components/Bible.js
@@ -1,26 +1,48 @@
-import { useEffect, useState } from "react";
-function Verses({ verses }) {
+import { useEffect, useState, useRef } from "react";
+function Verses({ verses, display, currentVerse }) {
+    // const centerChildVertically = (parentClientHeight, parentYPos, parentScrollHeight, childYPos) => {
+    //     let newChildPos = 0;
+    //     const parentMiddlePos = parentYPos + (parentClientHeight / 2);
+
+    //     return newChildPos;
+    // }
+
+    // position current verse to display view
+    useEffect(() => {
+        if (display) {
+            console.log(display.clientHeight);
+        }
+    }, [display]);
+
     if (verses.length === 0) {
         return <div className="verses-placeholder">Verses displays here</div>;
     }
-    return verses.map((value, index) => <div 
-        className="verses-item" 
+
+    return verses.map((value, index) => <div
+        className="verses-item"
         key={value.verseId}>
         <span>{index + 1}</span>
-        {value.content}  
+        {value.content}
     </div>);
 }
 export default function Bible() {
     const [verseInput, setVerseInput] = useState({ bookName: '', chapter: 0, verse: 0 });
     const [verses, setVerses] = useState([]);
     const [abbrevs, setAbbrevs] = useState([]);
+    const [currentVerse, setCurrentVerse] = useState({ id: '' });
+    const versesRef = useRef();
 
     useEffect(() => {
         fetch('https://localhost:7270/v1/books/abbreviations')
             .then(res => res.json())
-            .then(data => { setAbbrevs(data) })
+            .then(data => setAbbrevs(data))
             .catch(console.error);
-    }, []);
+
+        if (verses.length !== 0) {
+            setCurrentVerse({ id: verses.find(v => v.verseNo === verseInput.verse).verseId });
+        }
+
+    }, [verses]);
 
     // Resources
     const getChapterVerses = async (bookId, chapter) => await fetch(`https://localhost:7270/v1/books/${bookId}/verses?chapterNo=${chapter}`)
@@ -38,6 +60,8 @@ export default function Bible() {
 
         const bookId = abbrev.bookId;
         setVerses(await getChapterVerses(bookId, verseInput.chapter));
+        const verse = verses.find(v => v.verseNo === verseInput.verse);
+        console.log(verse);
     }
 
     function handleChange(e) {
@@ -51,8 +75,12 @@ export default function Bible() {
                 <input type="text" onChange={handleChange} name="verseInput" placeholder="Verse Search: ctrl + /" />
                 <input type="submit" id="submit" />
             </form>
-            <div className="verses">
-                <Verses verses={verses} />
+            <div className="verses" ref={versesRef}>
+                <Verses 
+                    verses={verses} 
+                    display={versesRef.current} 
+                    currentVerse={currentVerse}
+                />
             </div>
         </section>
     );
