28660d226b113f3de59b1371a18ceb58e1d8380c
Jared Teng <jaredteng1602@gmail.com>
2023-12-15 23:40:29 +1100

    update complete main functions for bible component

--- src/pages/components/Bible.js
+++ src/pages/components/Bible.js
@@ -1,117 +1,112 @@
 import { useEffect, useRef, useReducer, useState } from "react";
+
+/* 
+    Considerations:
+        - expensive re-renders
+        - caching
+        - security
+        - input validation    
+*/
+const initState = {
+    verseInput: { bookName: '', chapterNo: 0, verseNo: 0 },
+    verses: [],
+    abbrevs: [],
+    currentVerse: {}
+};
+
 function reducer(state, action) {
     switch (action.type) {
-        case 'verseInput': {
-            return { ...state, verseInput: action.verseInput };
-        }
-        case 'verses': {
-            return { ...state, verses: action.verses };
-        }
-        case 'abbrevs': {
-            return { ...state, abbrevs: action.abbrevs };
-        }
-        case 'currentVerse': {
-            return { ...state, currentVerse: action.currentVerse };
-        }
-        default:
-            return state;
+        case 'verseInput': return { ...state, verseInput: action.verseInput };
+        case 'verses': return { ...state, verses: action.verses };
+        case 'abbrevs': return { ...state, abbrevs: action.abbrevs };
+        case 'currentVerse': return { ...state, currentVerse: action.currentVerse };
+        default: return state;
     }
 }
 
-function Verses({ verses, currentVerse, display }) {
-    // centering verse in container view
-    const [box_ypos, setBox_ypos] = useState(0);
+function Verses({ verses, currentVerse }) {
     const verse = useRef(null);
-    const box_center_y_pos = (box) => {
-        const rect = box.getBoundingClientRect();
-        return setBox_ypos(rect.top + (rect.height / 2));
-    };
+    const verseDisplay = useRef(null);
+    const verseItems =
+        verses.map((value, index) => {
+            return <div
+                ref={value.verseId === currentVerse.verseId ? verse : null}
+                className={value.verseId === currentVerse.verseId ? "verses-current" : "verses-item"}
+                key={value.verseId}
+            >
+                <span>{index + 1}</span>
+                {value.content}
+            </div>
+        });
 
-    useEffect(() => {
-        if (display !== null && verse !== null) {
-            setBox_ypos(box_center_y_pos(display));
-        }
-    }, [display]);
-
-    // display when no verses
-    if (verses.length === 0) {
-        return <div className="verses-placeholder">Verses displays here</div>;
-    }
-
-    return verses.map((value, index) => {
-        if (Object.keys(currentVerse) !== 0 && value.verseId === currentVerse.verseId) {
-            // highlight current verse
-            return <div style={{
-                position: 'relative',
-                top: `${box_ypos}px`
-            }} ref={verse} className="verses-current" key={value.verseId}><span>{index + 1}</span>{value.content}</div>
-        }
-        return <div className="verses-item" key={value.verseId}><span>{index + 1}</span>{value.content}</div>;
-    });
+    return [
+        <div className="verses" ref={verseDisplay}> {verses.length === 0
+            ? <div className="verses-placeholder">Verses displays here</div> : verseItems
+        } </div>,
+        verse,
+        verseDisplay
+    ];
 }
+
 export default function Bible() {
-    const initState = {
-        verseInput: { bookName: '', chapterNo: 0, verseNo: 0 },
-        verses: [],
-        abbrevs: [],
-        currentVerse: {}
-    };
+    // States
+    const [currentInput, setCurrentInput] = useState({ bookName: '', chapterNo: 0, verseNo: 0 });
     const [state, dispatch] = useReducer(reducer, initState);
-    const versesRef = useRef(null);
-    const getChapterVerses = async (bookId, chapterNo) => await fetch(`https://localhost:7270/v1/books/${bookId}/verses?chapterNo=${chapterNo}`)
-        .then(res => res.json())
-        .catch(console.error);
+    const [verseItems, currentVerseRef, displayRef] = Verses({ verses: state.verses, currentVerse: state.currentVerse });
 
     useEffect(() => {
-        // STATE: abbrevs
+        // SET STATE: abbrevs
         if (state.abbrevs.length === 0) {
             fetch('https://localhost:7270/v1/books/abbreviations')
                 .then(res => res.json())
                 .then(data => dispatch({ type: 'abbrevs', abbrevs: data }))
                 .catch(console.error);
         }
-        
-        // STATE: currentVerse
+
+        // SET STATE: currentVerse
         const currentVerse = state.currentVerse;
         const verseInput = state.verseInput;
         const verses = state.verses;
         if (verses.length !== 0 && verseInput.verse !== 0 && Object.keys(currentVerse) !== 0) {
-            if (state.currentVerse.verseNo !== verseInput.verseNo) {
+            if (currentVerse.verseNo !== verseInput.verseNo || currentVerse.chapterNo !== verseInput.chapterNo) {
                 dispatch({ type: 'currentVerse', currentVerse: verses.find(v => v.verseNo === verseInput.verseNo) });
             }
         }
 
-    }, [state]);
+        // scroll verse into view
+        if (!Object.is(displayRef.current, null) && !Object.is(currentVerseRef.current, null)) {
+            displayRef.current.scrollTop = currentVerseRef.current.offsetTop;
+        }
+
+    }, [state, currentVerseRef, displayRef]);
 
     // form handlers
     async function handleSubmit(e) {
         e.preventDefault();
+        // SET STATE: verseInput
+        dispatch({ type: 'verseInput', verseInput: currentInput });
 
-        // STATE: verses
-        const verseInput = state.verseInput;
-        const abbrev = state.abbrevs.find(a => a.abbreviation.toLowerCase() === verseInput.bookName);
-        dispatch({ type: 'verses', verses: await getChapterVerses(abbrev.bookId, verseInput.chapterNo) });
+        // SET STATE: verses
+        const abbrev = state.abbrevs.find(a => a.abbreviation.toLowerCase() === currentInput.bookName);
+        dispatch({
+            type: 'verses', 
+            verses: await fetch(`https://localhost:7270/v1/books/${abbrev.bookId}/verses?chapterNo=${currentInput.chapterNo}`)
+                .then(res => res.json())
+                .catch(console.error)
+        });
     }
 
     function handleChange(e) {
-        // STATE: verseInput
+        // STATE: currentInput
         const input = e.target.value.toLowerCase().split(/\s+/);
-        dispatch({ type: 'verseInput', verseInput: { bookName: input[0], chapterNo: Number(input[1]), verseNo: Number(input[2]) } });
+        setCurrentInput({ bookName: input[0], chapterNo: Number(input[1]), verseNo: Number(input[2]) });
     }
 
-    return (
-        <section className="bible">
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} name="verseInput" placeholder="Verse Search: ctrl + /" />
-                <input type="submit" id="submit" />
-            </form>
-            <div className="verses" ref={versesRef}>
-                <Verses
-                    verses={state.verses}
-                    currentVerse={state.currentVerse}
-                    display={versesRef.current}
-                />
-            </div>
-        </section>
-    );
+    return <section className="bible">
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange} name="verseInput" placeholder="Verse Search: ctrl + /" />
+            <input type="submit" id="submit" />
+        </form>
+        {verseItems}
+    </section>;
 }
